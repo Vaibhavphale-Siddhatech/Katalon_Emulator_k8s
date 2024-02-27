4b3f96845ec7f4f2fee6874489b7a7675bddf9b0
Vaibhav Mohan Phale <81164352+Vaibhavphale-Siddhatech@users.noreply.github.com>
2024-02-27 16:16:49 +0530

    Update vnc_portforward.sh

--- vnc_portforward.sh
+++ vnc_portforward.sh
@@ -1,13 +1,25 @@
-sh "pwd"
-def pod = sh(returnStdout: true, script: "minikube kubectl -- get pods --no-headers -o custom-columns=':metadata.name'")
-echo "pod name ${pod}"
-def status
-    	            
-while(status != 'Running') {
-   echo "Sleeping for 5 seconds..."
-   sleep(5)
-   status = sh(returnStdout: true, script: "minikube kubectl -- get pods ${pod} --no-headers -o custom-columns=':status.phase'")
-   echo "status ${status}"
-}
-    	
-sh "minikube kubectl -- port-forward ${pod} 5900:5900"
+#!/bin/bash
+
+# Run a command to get the current working directory
+current_dir=$(pwd)
+echo "Current directory: ${current_dir}"
+
+# Run a command to get the list of pods
+pod=$(minikube kubectl -- get pods --no-headers -o custom-columns=':metadata.name')
+echo "Pod name: ${pod}"
+
+# Initialize status variable
+status=""
+
+# Loop until the status is 'Running'
+while [ "$status" != "Running" ]; do
+    echo "Sleeping for 5 seconds..."
+    sleep 5
+
+    # Get the status of the pod
+    status=$(minikube kubectl -- get pods ${pod} --no-headers -o custom-columns=':status.phase')
+    echo "Status: ${status}"
+done
+
+# Port forward using kubectl
+minikube kubectl -- port-forward ${pod} 5900:5900
